5e62bf512dc68c183346a156775b18e05dcc28c7
guntherhaas <guntherhaas.eng@gmail.com>
2024-06-14 16:56:40 +0200

    initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Gunther Masi Haas on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
